b5facc528702e710e4281e3885ef13985755a5de
Julian van der Horst <julianvanderhorst@pm.me>
2024-03-19 13:02:40 +0100

    Added nederwoon

--- main.py
+++ main.py
@@ -45,11 +45,56 @@ def extract_houses_max(soup):
         try:
             title = house.find_next('h2').text
             price = house.find('dd').text.replace('€', '').replace('p/m', '').replace(' ', '').replace(',', '.')
+            price = price.replace('.', '', price.count('.') - 1)
+
             location = house.find_next('p').text
             link = house['href']
             image = house.find('img')['src']
             verhuurd = house.select("div.orange.text-ice")
             full = len(verhuurd) > 0 and verhuurd[0].text == "Verhuurd"
+
+            houses.append({
+                'title': title,
+                'price': price,
+                'type': "unknown",
+                'location': location,
+                'image': image,
+                'full': full,
+                'link': link
+            })
+        except Exception as e:
+            print(e)
+            houses.append({
+                'title': 'unknown',
+                'price': 'unknown',
+                'type': 'unknown',
+                'location': 'unknown',
+                'image': 'unknown',
+                'full': False,
+                'link': 'unknown'
+            })
+    return houses
+
+
+def extract_houses_nederwoon(soup):
+    houses = []
+    for house in soup.find_all('div', class_='location'):
+        try:
+            text_block = house.find_all('div', class_='click-see-page-button')
+            text_block_ps = text_block[0].find_all('p')
+            if 'Garagebox' in text_block_ps[1].text or 'Parkeerplaats' in text_block_ps[1].text:
+                continue
+
+            title = text_block[0].find_next('a').text
+
+            price = text_block[1].find_next('p').text.replace('€', '').replace('p/mnd', '').replace(' ', '').replace(',', '.').replace('incl.', '').replace('excl.', '').strip()
+            price = price.replace('.', '', price.count('.') - 1)
+
+            location = 'Nijmegen'
+
+            link = text_block[0].find_next('a')['href']
+            image = 'https://www.nederwoon.nl' + house.find_next('img')['data-src']
+            full = False
 
             houses.append({
                 'title': title,
@@ -82,6 +127,8 @@ def extract_houses_wouw(soup):
             if len(house.select('div.pt-cv-ctf-prijs')) == 0:
                 continue
             price = house.find('div', class_="pt-cv-ctf-prijs").find_next('div').text
+            price = price.replace('.', '', price.count('.') - 1)
+
             location = 'Nijmegen'
             link = house.find('a')['href']
             image = house.find('a').find_next('img')['src']
@@ -109,6 +156,7 @@ def extract_houses_wouw(soup):
             })
     return houses
 
+
 def extract_houses_rotsvast(soup):
     houses = []
     for house in soup.find_all('div', class_='residence-gallery'):
@@ -118,6 +166,8 @@ def extract_houses_rotsvast(soup):
             full = "Verhuurd" in status or status == "Bezichtiging vol"
 
             price = house.find('div', class_='residence-price').text.replace('€', '').replace('p/mnd', '').replace(' ', '').replace(',', '.').replace('incl.', '').replace('excl.', '').strip()
+            price = price.replace('.', '', price.count('.') - 1)
+
             image_style = house.find('div', class_='residence-image')['style']
             style = cssutils.parseStyle(image_style)
             image = style['background-image'].replace('url(', '').replace(')', '')
@@ -159,6 +209,7 @@ def extract_houses_kbs(soup):
                 title = house.find_next('p').find_next('p').text
                 full = True
             price = house.find('div', class_='gb-container').find_all('p')[1].text
+            price = price.replace('.', '', price.count('.') - 1)
 
             sep = house.find('hr')
             image = house.find('img')['src']
@@ -197,6 +248,7 @@ def main():
                "=&balcony=&salvage=&parking_available=&sort-by=price-ascending")
     url_wouw = 'https://www.vdwouwvastgoedbeheer.nl/aanbod/'
     url_rotsvast = 'https://www.rotsvast.nl/woningaanbod/rotsvast-nijmegen/?type=2&office=RV013&maximumPrice[2]=900'
+    url_nederwoon = "https://www.nederwoon.nl/search?search_type=&type=&rooms=&completion=&sort=1&city=Nijmegen"
 
     while(True):
         houses = []
@@ -226,6 +278,13 @@ def main():
             response = requests.get(url_rotsvast)
             soup = BeautifulSoup(response.text, 'html.parser')
             houses += extract_houses_rotsvast(soup)
+        except Exception as e:
+            print(e)
+
+        try:
+            response = requests.get(url_nederwoon)
+            soup = BeautifulSoup(response.text, 'html.parser')
+            houses += extract_houses_nederwoon(soup)
         except Exception as e:
             print(e)
 
